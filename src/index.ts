9f0da553d536b4b47497ed27cecf619958df203c
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-13 13:26:06 +0900

    fix: import package.json with dynamic import

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,19 @@ import {
   renderHeader as renderHeaderBase,
   renderValidationErrors as renderValidationErrorsBase
 } from 'gunshi/renderer'
-import path from 'node:path'
-import { URL } from 'node:url'
 import pc from 'picocolors'
 import { commands } from './commands/index.js'
 import { default as show } from './commands/show.js'
-import { fail, readPackageJson } from './utils.js'
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
+import { fail } from './utils.js'
 
 async function main() {
-  const pkgJson = await readPackageJson(path.resolve(__dirname, '../package.json'))
+  const pkgJson = await import('../package.json', { with: { type: 'json' } })
 
-  await cli<typeof show.options>(process.argv.slice(2), show, {
+  await cli(process.argv.slice(2), show, {
     name: 'pnpmc',
     cwd: process.cwd(),
-    description: pkgJson.description as string,
-    version: pkgJson.version as string,
+    description: pkgJson.description,
+    version: pkgJson.version,
     subCommands: commands,
     renderHeader: async ctx => pc.cyanBright(await renderHeaderBase(ctx)),
     renderValidationErrors: async (ctx, e) => {
