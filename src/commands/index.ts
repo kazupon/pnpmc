b861acbbcd8d26d7f10bd5e5b5d679541245bfa2
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-13 13:04:01 +0900

    refactor: bump gunshi (#31)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,13 +1,5 @@
 import { interopDefault } from '@kazupon/jts-utils'
 
-import type { ArgOptions, Command, LazyCommand } from 'gunshi'
-
-export const commands = new Map<string, Command<ArgOptions> | LazyCommand<ArgOptions>>()
-commands.set(
-  'show',
-  (async () => await interopDefault(import('./show.js'))) as unknown as LazyCommand<ArgOptions>
-)
-commands.set(
-  'register',
-  (async () => await interopDefault(import('./register.js'))) as unknown as LazyCommand<ArgOptions>
-)
+export const commands = new Map()
+commands.set('show', async () => await interopDefault(import('./show.js')))
+commands.set('register', async () => await interopDefault(import('./register.js')))
